c5e577930bd07183b37973a3735c377fb4f29481
isofinly <isofinly@protonmail.com>
2022-10-20 11:58:19 +0300

    Carousel on the main page update

---
+++ projectapp/src/components/MainPageCarousel/MainPageCarousel.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import './MainPageCarousel.scss'
+
+function MainPageCarousel() {
+  return (
+    <div class="main__page__carousel--maindiv">
+  <div class="main__page__carousel--subdiv">
+    <div class="main__page__carousel--images"></div>
+  </div>
+  <div class="main__page__carousel--dots">
+    <div class="main__page__carousel--dot-1"></div>
+    <div class="main__page__carousel--dot-2"></div>
+    <div class="main__page__carousel--dot-3"></div>
+  </div>
+</div>
+  );
+}
+
+export default MainPageCarousel;
--- projectapp/src/components/index.js
+++ projectapp/src/components/index.js
@@ -1,3 +1,4 @@
 export {default as StartPageIcon} from './StartPageIcon/StartPageIcon';
 export {default as MainPageText} from './MainPageText/MainPageText';
 export {default as BottomNavigation} from './BottomNavigation/BottomNavigation';
+export {default as MainPageCarousel} from './MainPageCarousel/MainPageCarousel';
