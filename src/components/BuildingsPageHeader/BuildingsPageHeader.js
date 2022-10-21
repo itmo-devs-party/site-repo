75686cef9e3bd840744f14de36b169b82ebb45db
isofinly <isofinly@protonmail.com>
2022-10-21 18:47:27 +0300

    Buildings class names commit

--- src/components/BuildingsPageHeader/BuildingsPageHeader.js
+++ src/components/BuildingsPageHeader/BuildingsPageHeader.js
@@ -7,72 +7,12 @@ function BuildingsPageHeader() {
       <div className="buildings__page__header-container">
       <div class="e445_9457"><span  class="e445_9458">Buildings</span></div>
       <div class="e450_3511">
-        <div class="e450_3512"></div>
-        <div class="e450_3513">
-          <div class="e450_3514">
-            <div class="e450_3516"></div>
-            <div class="e450_3517"></div>
-            <div class="e450_3518">
-              <div class="e450_3519"></div>
-              <div class="e450_3520"></div>
-              <div class="e450_3521"></div>
-              <div class="e450_3522"></div>
-              <div class="e450_3523"></div>
-              <div class="e450_3524"></div>
-              <div class="e450_3525"></div>
-              <div class="e450_3526"></div>
-              <div class="e450_3527"></div>
-            </div>
-            <div class="e450_3528"></div>
-            <div class="e450_3529">
-              <div class="e450_3531">
-                <div class="e450_3532"></div>
-                <div class="e450_3533">
-                  <div class="e450_3534"></div>
-                  <div class="e450_3535"></div>
-                  <div class="e450_3536"></div>
-                  <div class="e450_3537"></div>
-                  <div class="e450_3538"></div>
-                  <div class="e450_3539"></div>
-                  <div class="e450_3540"></div>
-                  <div class="e450_3541"></div>
-                  <div class="e450_3542">
-                    <div class="e450_3543"></div>
-                    <div class="e450_3544"></div>
-                  </div>
-                </div>
-              </div>
-              <div class="e450_3545"></div>
-              <div class="e450_3546"></div>
-              <div class="e450_3547"></div>
-              <div class="e450_3548"></div>
-              <div class="e450_3549"></div>
-              <div class="e450_3550"></div>
-              <div class="e450_3551"></div>
-            </div>
-            <div class="e450_3552">
-              <div class="e450_3553">
-                <div class="e450_3554"></div>
-                <div class="e450_3555"></div>
-                <div class="e450_3556"></div>
-                <div class="e450_3557"></div>
-                <div class="e450_3558"></div>
-                <div class="e450_3559"></div>
-                <div class="e450_3560"></div>
-                <div class="e450_3561"></div>
-                <div class="e450_3562"></div>
-              </div>
-              <div class="e450_3563"></div>
-              <div class="e450_3564"></div>
-            </div>
-            <div class="e450_3565"></div>
-          </div>
-        </div>
+        <div class="e450_3565"></div>
       </div>
       <div class="e445_9455">
-        <div class="e445_9456"><span  class="ei445_9456_44_1901">Find Cousre</span>
-          <div class="ei445_9456_44_1913">
-            <div class="ei445_9456_44_1913_44_1910"></div>
+        <div class="e445_9456"><span  class="buildings__page__header-search-text">Find Cousre</span>
+          <div class="buildings__page__header-search-icon-div">
+            <div class="buildings__page__header-search-icon"></div>
           </div>
         </div>
       </div>
