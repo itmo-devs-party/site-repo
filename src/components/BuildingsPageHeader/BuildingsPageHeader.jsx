7139cdb791e7ed6283f38f0f859589c81d55af96
isofinly <isofinly@protonmail.com>
2022-10-24 01:56:50 +0300

    Cleared out bad classes and added new for seartch

--- src/components/BuildingsPageHeader/BuildingsPageHeader.jsx
+++ src/components/BuildingsPageHeader/BuildingsPageHeader.jsx
@@ -5,24 +5,21 @@ import './BuildingsPageHeader.scss';
 function BuildingsPageHeader() {
     return (
       <div class="buildings__page__header-subcontainer">
-      <div class="e445_9457">
-        <span  class="e445_9458">Buildings</span></div>
-      <div class="e450_3511">
-        <div class="e450_3565"></div>
+      <div class="searchInput__Container-heading-container">
+        <span  class="searchInput__Container-heading">Buildings</span></div>
+      <div class="searchInput__Container-logo">
+        <div class="searchInput-logo"></div>
       </div>
-      <div class="e445_9455">
+
+      {/* you can skip the block beneath and forget about it. it is heritage from the fisrt iteraion of the page */}
+
+      {/* <div class="searchInput__Container-search">
         <div class="e445_9456"><span  class="buildings__page__header-search-text">Find Cousre</span>
           <div class="buildings__page__header-search-icon-div">
             <div class="buildings__page__header-search-icon"></div>
           </div>
         </div>
-      </div>
-      <div class="buildings__page__header-queries-div">
-        <div class="e450_3566"></div>
-        <div class="e445_9283">
-          <span  class="ei445_9283_44_1928">Popular queries</span>
-        </div>
-      </div>
+      </div> */}
     </div>
     );
 };
