ff1318f5377102bb9062775403aed85f6b03ba29
isofinly <isofinly@protonmail.com>
2022-10-24 01:57:23 +0300

    Added search

--- src/pages/Buildings/Buildings.js
+++ src/pages/Buildings/Buildings.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+// eslint-disable-next-line
 import { BuildingsList, BuildingsPageHeader } from "../../components";
+import {data} from '../../constants'
 import './Buildings.scss';
 
 const Buldings = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div class = 'buildings__page-main-container'>
-      <div class='buildings__page-header-container'>
-      <BuildingsPageHeader />
-      <div classx='buildings__page-list-container'>
-      <BuildingsList />
+    <>
+      <div className="buildingsContainer">
+        <div className="searchInput_Container">
+          <BuildingsPageHeader/>
+          <input class="searchInput__Container-search" type="text" placeholder="Search here..." onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }} />
+          
+        </div>
+        <div className="buildingsList_Container">
+          {
+            data 
+              .filter((val) => {
+                if(searchTerm == ""){
+                  return val;
+                }else if(val.adress.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val;
+                }
+              })
+              .map((val) => {
+                return(
+                  <div className="building_div" key={val.id}>
+                      <img src={val.image} alt="" />
+                      <h3>{val.adress}</h3>
+                      <p className="person">{val.person}</p>
+                      <a className ='building_div-link' href={val.link}>See more</a>
+                  </div> 
+                )
+              })
+          }
+        </div>
       </div>
-      </div>
-      
-    </div>
-  );
-};
+    </>
+  )
+}
 
 export default Buldings;
