c3c922b57c525a18ac7eba98b137d52f18832459
isofinly <isofinly@protonmail.com>
2022-10-24 11:37:06 +0300

    Made a blank page for people

---
+++ src/pages/People/People.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './People.scss'
+
+// create a component called People
+
+const People = () => {
+  return (
+    <div className="People">
+      <h1>People</h1>
+    </div>
+  );
+}
+export default People;
