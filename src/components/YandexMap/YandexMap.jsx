e02ad5d60a7366f0ce98873e7290b0550c798313
isofinly <isofinly@protonmail.com>
2022-11-25 02:01:47 +0300

    coordinates update

--- src/components/YandexMap/YandexMap.jsx
+++ src/components/YandexMap/YandexMap.jsx
@@ -11,28 +11,47 @@ const YandexMap = () =>
   <div style={{width: '100%', height: 'auto' ,position: 'relative', margin: '0px 0 0 0' }}>
             <Box sx={{ display: 'flex', justifyContent: 'center', margin: '0 0 0px 0', height: { xs:500, md: 700 }}}>
                       <Map width='100%' height='auto' defaultState={{
-                      center: [55.75, 37.57],
-                      zoom: 9,
+                      center: [59.9490959,30.3085916],
+                      zoom: 12,
                       controls: ['zoomControl', 'fullscreenControl']
                     }}>
                         <Placemark
-                        defaultGeometry={[55.75, 37.57]} 
+                        defaultGeometry={[59.956386, 30.309993]} 
                         properties={{
-                        balloonContentBody: 'This is balloon loaded by the Yandex.Maps API module system'
+                        balloonContentBody: ' Университет ИТМО на Кронверксом 49'
                     }}  
                         />
                         <Placemark
-                        defaultGeometry={[55.79, 37.59]} 
+                        defaultGeometry={[59.9269688,30.2679992]} 
                         properties={{
-                        balloonContentBody: 'This is balloon loaded by the Yandex.Maps API module system'
+                          balloonContentBody: ' Университет ИТМО на Ломоносова 9'
                     }}  
                         />
                         <Placemark
-                        defaultGeometry={[55.71, 37.50]} 
+                        defaultGeometry={[59.9728756,30.299376]} 
                         properties={{
-                        balloonContentBody: 'This is balloon loaded by the Yandex.Maps API module system'
+                        balloonContentBody: ' Вяземский пер., 5/7'
                     }}  
                         />
+                        <Placemark
+                        defaultGeometry={[59.9423948,30.2947174]} 
+                        properties={{
+                        balloonContentBody: 'Биржевая линия, 4'
+                    }}  
+                        />
+                        <Placemark
+                        defaultGeometry={[59.92948,30.3128744]} 
+                        properties={{
+                        balloonContentBody: 'пер. Гривцова, 14-16'
+                    }}  
+                        />
+                        <Placemark
+                        defaultGeometry={[59.9462409,30.340817]} 
+                        properties={{
+                        balloonContentBody: 'ул. Чайковского, 11/2'
+                    }}  
+                        />
+
                 </Map>
             </Box>
         </div>
